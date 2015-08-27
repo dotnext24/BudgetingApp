17012e593a230b3215434e0d25f23a48e26cbe61
dotnext24 <dotnext24@gmail.com>
2015-08-27 15:20:35 +0530

    RegisterViewModel

--- BudgetingApp/WebUI/Models/AccountViewModels.cs
+++ BudgetingApp/WebUI/Models/AccountViewModels.cs
@@ -50,6 +50,18 @@ namespace WebUI.Models
         public string UserName { get; set; }
 
         [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Required]
+        [DataType(DataType.PhoneNumber)]
+        public string Phone { get; set; }
+
+        [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
         [DataType(DataType.Password)]
         [Display(Name = "Password")]
